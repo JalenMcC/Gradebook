024b621a2ab39167c3d1a5c9b4b63a6d7ed4ea22
JalenMcC <76828457+JalenMcC@users.noreply.github.com>
2021-12-19 15:31:49 -0500

    initial commit

---
+++ src/GUIComponents/BasicFrame.java
@@ -0,0 +1,19 @@
+package GUIComponents;
+
+import javax.swing.JFrame;
+
+
+public class BasicFrame {
+
+	JFrame frame;
+	
+	BasicFrame() {
+		
+		frame = new JFrame();
+		
+		
+		
+		frame.setVisible(true);
+	}
+	
+}
