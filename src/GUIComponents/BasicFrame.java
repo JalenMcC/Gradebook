21c6b96b0dd8053751ed82c95ee18803571580c8
JalenMcC <76828457+JalenMcC@users.noreply.github.com>
2021-12-22 18:17:18 -0500

    Created the base frame for the website (WIP)
    For this commit, I created an edited version of the base frame. I will use this class, which extends JFrame, instead of JFrame. I am not happy with the design at the moment so it is still a work in progress.

--- src/GUIComponents/BasicFrame.java
+++ src/GUIComponents/BasicFrame.java
@@ -1,17 +1,85 @@
 package GUIComponents;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BorderFactory;
 
 public class BasicFrame {
+	
+	//Creates the default window frame for the program
 
 	JFrame frame;
+	JPanel titleSection, bodySection, footerSection;
+	JPanel centerBody, centerTitle;
+	JLabel title;
 	
-	BasicFrame() {
+	public BasicFrame() {
 		
+		// Initializes the default frame
 		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(true);
+		frame.setSize(500, 500);
+		//frame.getContentPane().setBackground(new Color(0, 38, 77)); //Navy Blue ish
 		
 		
+		//Creates panels to divide the frame into sections
+		titleSection = new JPanel();
+		bodySection = new JPanel();
+		footerSection = new JPanel();
+		centerBody = new JPanel();
+		centerTitle = new JPanel();
+		
+		//TODO: Remove this section later, 
+		//		just using as guidelines
+		//titleSection.setBackground(Color.white);
+		//bodySection.setBackground(Color.darkGray);
+		//footerSection.setBackground(Color.lightGray);
+		
+		titleSection.setBackground(new Color(0, 38, 77));
+		bodySection.setBackground(new Color(0, 38, 77));
+		footerSection.setBackground(new Color(0, 38, 77));
+		centerTitle.setBackground(Color.white);
+		
+		//Sets the size of the guideline panels
+		titleSection.setPreferredSize(new Dimension(0, 100));	//125??
+		bodySection.setPreferredSize(null);
+		footerSection.setPreferredSize(new Dimension(0, 75));
+		
+		//TODO: testing stuff
+		//centerBody.setBackground(Color.red);
+		//centerBody.setPreferredSize(new Dimension(0, 100));
+		
+		//Main "Gradebook" Title Text
+		title = new JLabel("Gradebook");
+		title.setFont(new Font("Monospaced", Font.BOLD, 50));
+		title.setForeground(Color.black);
+		title.setHorizontalTextPosition(JLabel.CENTER);
+		title.setVerticalTextPosition(JLabel.BOTTOM);
+		//title.setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25));
+		
+		frame.add(titleSection, BorderLayout.NORTH);
+		frame.add(bodySection, BorderLayout.CENTER);
+		frame.add(footerSection, BorderLayout.SOUTH);
+		
+		//centerTitle
+		titleSection.add(centerTitle);
+		centerTitle.add(title);
+		
+		bodySection.setLayout(new BorderLayout());
+		//horizontalGap, verticalGap
+		
+		bodySection.add(centerBody, BorderLayout.CENTER);
+		bodySection.setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25));
+		titleSection.add(centerTitle, BorderLayout.CENTER);
+		titleSection.setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25));
 		
 		frame.setVisible(true);
 	}
--- src/mainCode/Main.java
+++ src/mainCode/Main.java
@@ -1,9 +1,12 @@
 package mainCode;
 
+import GUIComponents.BasicFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		new BasicFrame();
 
 	}
 
